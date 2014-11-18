34d8e3c38da25e486685f5d5205e5f7785825b69
SejongPark <devsejong@gmail.com>
2014-11-18 18:44:52 +0900

    첫번째 커밋

---
+++ src/test/java/kr/sadalmelik/ArgsTest.java
@@ -0,0 +1,19 @@
+package kr.sadalmelik;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+
+public class ArgsTest {
+    @Test
+    public void argsTest() throws Exception {
+        Args arg = new Args("l,p#,d*", new String[]{"-l", "-p", "2000", "-d", "/test/test"});
+        assertThat(arg.getBoolean('l'), is(true));
+        assertThat(arg.getInt('p'), is(2000));
+        assertThat(arg.getString('d'), is("/test/test"));
+    }
+
+
+}
